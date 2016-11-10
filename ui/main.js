c7dd37a91123b701102ef6364fa7441c2f134668
T3lo <ritwik97bhattacharya@gmail.com>
2016-11-10 09:33:45 +0000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,12 @@
 console.log('Loaded!');
 
 //Move th image
+
 var img = document.getElementById('profile-pic');
 var marginLeft = 0;
+
 function moveRight() {
-    marginLeft += 10;
+    marginLeft = marginLeft + 10;
     img.style.marginleft = marginLeft + 'px';
 }
 img.onclick = function () {
