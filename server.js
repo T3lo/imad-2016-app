ee798a1256380194e9c6f89ede3d0a3a2ba9c90b
T3lo <ritwik97bhattacharya@gmail.com>
2016-11-10 10:07:27 +0000

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -74,11 +74,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-var counter = 0;
-app.get('/c', function( req, res) {
-   counter = counter + 1;
-   res.send(counter.toString());
-});
+
 
 app.get('/:articleName', function(req, res) {
   var articleName = req.params.articleName;
