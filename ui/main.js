53baae88460764c4047d1ec763fd22659f03d2ee
T3lo <ritwik97bhattacharya@gmail.com>
2016-11-10 09:38:47 +0000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,5 +10,5 @@ function moveRight() {
     img.style.marginleft = marginLeft + 'px';
 }
 img.onclick = function () {
-    var interval = setInterval(moveRight, 100);
+    img.style.marginleft = '100px';
 };
