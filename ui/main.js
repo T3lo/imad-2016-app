5cae57e75c59b29eba9d1a4022bab16e37ef3e8e
T3lo <ritwik97bhattacharya@gmail.com>
2016-11-10 10:18:40 +0000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,14 +1,16 @@
-console.log('Loaded!');
+//Counter code
+var button = document.getElementById('counter');
+var counter = 0;
 
-//Move th image
-
-var img = document.getElementById('profile-pic');
-var marginLeft = 0;
-
-function moveRight() {
-    marginLeft = marginLeft + 1;
-    img.style.marginLeft = marginLeft + 'px';
-}
-img.onclick = function () {
-    var interval = setInterval(moveRight, 50);    
+button.onclick = function () {
+  
+  //Make a request to the counter endpoint
+  
+  //capture the response and store it in a variable 
+  
+  //render the variable in the correct span
+  counter = counter + 1;
+  var span = document.getElementById('count');
+  span.innerHtml = counter.toString();
+  
 };
