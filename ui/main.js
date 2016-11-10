2513fa81a3628d21f4886ef1cdb13012c52f87d5
T3lo <ritwik97bhattacharya@gmail.com>
2016-11-10 10:41:53 +0000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,16 +1,22 @@
 //Counter code
 var button = document.getElementById('counter');
-var counter = 0;
 
 button.onclick = function () {
   
-  //Make a request to the counter endpoint
+  //create a request to the counter endpoint
+  var request = new XMLHttpRequest();
   
   //capture the response and store it in a variable 
+  request.onreadystatechange = function(){
+      if(request.readyState === XMLHttprequest.DONE){
+          if(request.status === 200){
+              var counter = request.responseText;
+              var span = document.getElementById('count');
+              span.innerHTML = counter.toString();
+          }
+      }
+  };
   
-  //render the variable in the correct span
-  counter = counter + 1;
-  var span = document.getElementById('count');
-  span.innerHTML = counter.toString();
-  
+  request.open('GET', 'http://t3lo.imad.hasura-app.io/counter', true);
+  request.send(null);
 };
