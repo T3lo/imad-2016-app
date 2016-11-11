fe657971318258047f75eac2c485024146adf002
T3lo <ritwik97bhattacharya@gmail.com>
2016-11-11 03:35:19 +0000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -26,3 +26,23 @@ submit.onclick = function() {
   request.send(null);      
 
 };
+
+
+var button = document.getElementById('counter');
+button.onclick = function() {
+  var request = new XMLHttpRequest()  ;
+  
+  request.onreadystatechange = function() {
+    if(request.readyState === XMLHttpRequest.DONE) {
+        if(request.status === 200){
+            var res = request.responseText ;
+            var span = document.getElementById('count') ;
+            span.innerHTML = res ;
+        }
+    }
+  };
+  
+  request.open('GET', 'http://t3lo.imad.hasura-app.io/counter' , true);
+  request.send(null);
+  
+};
