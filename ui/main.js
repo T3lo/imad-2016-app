767ea0686dba59a32a564f8f575f784052dcaf8c
T3lo <ritwik97bhattacharya@gmail.com>
2016-11-11 03:22:09 +0000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,28 +1,3 @@
-//Counter code
-var button = document.getElementById('counter');
-
-button.onclick = function () {
-  
-  //create a request Object
-  var request = new XMLHttpRequest();
-  
-  //capture the response and store it in a variable 
-  request.onreadystatechange = function(){
-      if(request.readyState === XMLHttpRequest.DONE){
-          if(request.status === 200){
-              var counter = request.responseText;
-              var span = document.getElementById('count');
-              span.innerHTML = counter.toString();
-          }
-      }
-  };
-  
-  request.open('GET', 'http://t3lo.imad.hasura-app.io/counter', true);
-  request.send(null);
-};
-
-//Submit name
-
 var submit = document.getElementById('submit_btn');
 
 submit.onclick = function() {
@@ -45,48 +20,8 @@ submit.onclick = function() {
       }
   } ;
   
-  var nameInput = document.getElementById('name');
-  var name = nameInput.value;
-  request.open('GET', 'http://t3lo.imad.hasura-app.io/submit-name?name=' + name, true);
-  request.send(null);      
-
-};
-
-
-
-
-
-
-
-
-
-
-
-
-var submit = document.getElementById('submit_btn');
-
-submit.onclick = function() {
-  //create a request Object
-  var request = new XMLHttpRequest();
-  
-  //capture the response and store it in a variable 
-  request.onreadystatechange = function(){
-      if(request.readyState === XMLHttpRequest.DONE){
-          if(request.status === 200){
-             var names = request.responseText;
-             names = JSON.parse(names);
-             var list = '';
-            for(var i=0;i<names.length;i++){
-                list += '<li>' + names[i] + '</li>';
-            }
-            var ul=document.getElementById('namelist');
-            ul.innerHTML = list;
-          }
-      }
-  } ;
-  
-  var nameInput = document.getElementById('name');
-  var name = nameInput.value;
+  var field = document.getElementById('name');
+  var name = field.value;
   request.open('GET', 'http://t3lo.imad.hasura-app.io/submit-name?name=' + name, true);
   request.send(null);      
 
