b54993ac9f92173a0b9b6e56af0ddeb6e31d9ab1
T3lo <ritwik97bhattacharya@gmail.com>
2016-11-12 12:01:00 +0000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -28,7 +28,7 @@ body.onload = function() {
                 var txt = request.responseText;
                 txt = JSON.parse(txt);
                 var div = document.getElementById('test');
-                div.innerHTML = txt["id"];
+                div.innerHTML = createTemplate(txt);
             }
         }
     };
