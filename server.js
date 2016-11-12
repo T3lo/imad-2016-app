c56f54987afc44332c4aabd80e395e38f2ff1a18
T3lo <ritwik97bhattacharya@gmail.com>
2016-11-12 11:42:33 +0000

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -79,8 +79,8 @@ app.get('/div', function(req ,res) {
           res.status(500).send(err.toString());
       }
       else{
-    //      res.send(JSON.stringify(result.rows));
-            res.send(result.rows);
+          res.send(JSON.stringify(result.rows));
+     //       res.send(result.rows);
       }
    });  
   
