63cececfb5486e43e98f419e9050c357d9fdb64a
T3lo <ritwik97bhattacharya@gmail.com>
2016-11-12 07:28:33 +0000

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -110,7 +110,7 @@ app.get('/articles/:articleName', function(req, res) {
              res.status(404).send('Article not found');
          } else {
 //                      res.send(JSON.stringify(result.rows));
-             var articleData = result.rows;
+             var articleData = result.rows[0];
              res.send(createTemplate(articleData));
          }
      }
