d3d0ab72edb6f72fa57c74895afacc9129dadc61
T3lo <ritwik97bhattacharya@gmail.com>
2016-11-13 04:47:44 +0000

    [imad-console] Updates ui/main2.js

--- ui/main2.js
+++ ui/main2.js
@@ -1,1 +1,26 @@
+function call (i) {
+    var div = document.getElementById('test');
+    div.innerHTML = '<p>qwerty</p>';
+
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if( request.readyState === XMLHttpRequest.DONE) {
+            if( request.status === 200 ) {
+                var txt = request.responseText;
+                txt = JSON.parse(txt);
+div.innerHTML += "<div><div id='t1'><p>"+txt["date"]+"</p></div><div id='t2'><img id='shot' src="+txt["link"]+"/><p>"+txt["title"];
+div.innerHTML += "</p></div><div id='t3'><p></p></div></div>";
+            }
+        }
+    };
+        request.open('GET', 'http://t3lo.imad.hasura-app.io/div/'+i, true);
+        request.send(null);
+}
+
+var body = document.getElementById('set');
+body.onload = function() {
+    for(var i=1;i<=8;i++) {
+        call(i);
+    }
+};
 
