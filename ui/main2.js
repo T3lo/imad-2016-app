46824ba989b85327dd1fa20d943ac02a82fa7635
T3lo <ritwik97bhattacharya@gmail.com>
2016-11-13 06:08:55 +0000

    [imad-console] Updates ui/main2.js

--- ui/main2.js
+++ ui/main2.js
@@ -8,7 +8,7 @@ function call (i) {
             if( request.status === 200 ) {
                 var txt = request.responseText;
                 txt = JSON.parse(txt);
-div.innerHTML+="<div><div id='t1'><p>"+txt["date"]toDateString()+"</p></div><div id='t2'><img id='shot' src='"+txt["link"]+"'/><h1>";
+div.innerHTML+="<div><div id='t1'><p>"+txt["date"].toDateString()+"</p></div><div id='t2'><img id='shot' src='"+txt["link"]+"'/><h1>";
 div.innerHTML += txt["title"]+"</h1></div><div id='t3'></div></div>";
             }
         }
