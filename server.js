5b02fda537b887f17726c968875adf0c31040fd3
T3lo <ritwik97bhattacharya@gmail.com>
2016-11-26 23:10:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -125,7 +125,7 @@ app.get('/one/comments' ,function(req ,res ){
 
 
 app.get('/Blog/:articleName', function(req, res) {
-  pool.query("SELECT * FROM article WHERE title = $1", [req.params.articleName], function(err , result) {
+  pool.query("SELECT * FROM article WHERE id = $1", [req.params.articleName], function(err , result) {
      if(err) {
          res.status(500).send(err.toString());
      } 
