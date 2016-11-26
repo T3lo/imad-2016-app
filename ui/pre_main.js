c5a5762583a4dc8e4ea08344f8b7004494c1d883
T3lo <ritwik97bhattacharya@gmail.com>
2016-11-26 12:49:30 +0530

    [imad-console] Updates ui/pre_main.js

--- ui/pre_main.js
+++ ui/pre_main.js
@@ -1,3 +1,27 @@
+function createTemplate (data) {
+
+var htmlTemplate=`
+<div id='places'>
+         <div id='t1'><p>11 Nov, 2016</p></div>
+         <div id='t2'>
+<img id='shot' src='http://www.tripdesigners.co/wp-content/uploads/2015/09/635693880042525992-518822648_couple-hiking-valley.jpg'/>
+<p>this is the body of the article in the blog.Anyway this is a
+sample and wont see the light of corporate business.
+</p></div>
+         <div id='t3'>
+             <div id='comment'><span class='no'>25</span><p>comments</p></div>
+             <div id='like'>
+                <div class='no'><span>281</span></div>
+                <div class='like_pic'><img src='https://cdn2.iconfinder.com/data/icons/cute-tech-icon-set-1/512/Like-128.png'/></div>
+             </div>
+          </div>
+     </div>
+
+`;
+
+    return htmlTemplate;
+}
+
 
 function call (i) {
     var div = document.getElementById('test');
@@ -9,8 +33,6 @@ function call (i) {
             if( request.status === 200 ) {
                 var txt = request.responseText;
                 txt = JSON.parse(txt);
-div.innerHTML += "<div><div id='t1'><p>"+txt["date"]+"</p></div><div id='t2'><img id='shot' src="+txt["link"]+"/><p>"+txt["field"];
-div.innerHTML += "</p></div><div id='t3'><p></p></div></div>";
             }
         }
     };
@@ -20,10 +42,12 @@ div.innerHTML += "</p></div><div id='t3'><p></p></div></div>";
 
 var body = document.getElementById('set');
 body.onload = function() {
-    for(var i=1;i<=8;i++) {
-        call(i);
+    for(var i=1;i<=1;i++) {
+        div.innerHTML = createTemplate ();
     }
 };
+
+
 
 var submit = document.getElementById('submit_btn');
 
