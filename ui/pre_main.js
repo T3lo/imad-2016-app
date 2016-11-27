22518e71e7961d130f0e05fb0e9de5bc549ae20a
T3lo <ritwik97bhattacharya@gmail.com>
2016-11-27 10:58:04 +0530

    [imad-console] Updates ui/pre_main.js

--- ui/pre_main.js
+++ ui/pre_main.js
@@ -76,7 +76,7 @@ function call (i) {
                 }
                 cntr++;
                 if(cntr%20 === 0) {
-                    height += 2500;
+                    height += 1500;
                     main_div.style.height = height.toString() + "px";
                 }
             }
