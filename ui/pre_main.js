d9eb21aca25eb4a6da5272c300228abb6dd0bcf3
T3lo <ritwik97bhattacharya@gmail.com>
2016-11-27 10:55:20 +0530

    [imad-console] Updates ui/pre_main.js

--- ui/pre_main.js
+++ ui/pre_main.js
@@ -89,7 +89,7 @@ function call (i) {
 
 var body = document.getElementById('set');
 body.onload = function() {
-    for(var j=0;j<5;j++) {
+    for(var j=0;j<10;j++) {
         for(var i=1;i<=5;i++) {
             call(i);
         }
