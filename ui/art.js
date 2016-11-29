5f4ee9ce60b75d8f08f9d7175150506c4bd65b5a
T3lo <ritwik97bhattacharya@gmail.com>
2016-11-29 16:36:54 +0530

    [imad-console] Updates ui/art.js

--- ui/art.js
+++ ui/art.js
@@ -2,7 +2,8 @@ var art_ht = window.getComputedStyle( document.getElementById('article')).height
 var main_ht = window.getComputedStyle( document.getElementById('main')).height;
 var h=2000;
 
-if( parseInt(art_ht) > parseInt(main_ht) -770 ){ 
+while( parseInt(art_ht) > parseInt(main_ht) -770 ){ 
 	h += 1000;
 	document.getElementById('main').style.height = h.toString() + "px";
+main_ht = window.getComputedStyle(document.getElementById('main')).height;
 }
