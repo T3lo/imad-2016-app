5ac8b856a11a88abde83a5989fdde7be7278145e
T3lo <ritwik97bhattacharya@gmail.com>
2016-12-02 15:04:48 +0530

    [imad-console] Updates ui/pre_main.js

--- ui/pre_main.js
+++ ui/pre_main.js
@@ -99,57 +99,6 @@ body.onload = function() {
 };
 
 
-
-var submit = document.getElementById('submit_btn');
-
-submit.onclick = function() {
-  //create a request Object
-  var request = new XMLHttpRequest();
-  
-  //capture the response and store it in a variable 
-  request.onreadystatechange = function(){
-      if(request.readyState === XMLHttpRequest.DONE){
-          if(request.status === 200){
-             var names = request.responseText;
-             names = JSON.parse(names);
-             var list = '';
-            for(var i=0;i<names.length;i++){
-                list += '<li>' + names[i] + '</li>';
-            }
-            var ul=document.getElementById('namelist');
-            ul.innerHTML = list;
-          }
-      }
-  } ;
-  
-  var field = document.getElementById('name');
-  var name = field.value;
-  request.open('GET', 'http://t3lo.imad.hasura-app.io/submit-name?name=' + name, true);
-  request.send(null);      
-
-};
-
-
-var button = document.getElementById('counter');
-button.onclick = function() {
-  var request = new XMLHttpRequest()  ;
-  
-  request.onreadystatechange = function() {
-    if(request.readyState === XMLHttpRequest.DONE) {
-        if(request.status === 200){
-            var res = request.responseText ;
-            var span = document.getElementById('count') ;
-            span.innerHTML = res ;
-        }
-    }
-  };
-  
-  request.open('GET', 'http://t3lo.imad.hasura-app.io/counter' , true);
-  request.send(null);
-  
-};
-
-
 var btn = document.getElementById('submit_btn');
 btn.onclick = function () {
 
