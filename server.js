3ac6d23d19193fbabbc8b161b131b9c6ad84edba
T3lo <ritwik97bhattacharya@gmail.com>
2016-12-02 14:30:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -94,21 +94,21 @@ app.get('/hash/:input', function(req, res) {
         res.send(hashString);
 });
  
-app.post('/create-user', function (req ,res) {
-    var username = req.body.username;
-    var password = req.body.password;
-    
-    var salt = crypto.randomBytes(128).toString('hex');
-    var dbString = hash(password, salt);
-    pool.query('Insert into "user" (username,password) Values ($1, $2) ', [username, dbString], function(err, result) {
-        if(err) {
+app.post('/create-user', function (req, res) {
+   // username, password
+   // {"username": "tanmai", "password": "password"}
+   // JSON
+   var username = req.body.username;
+   var password = req.body.password;
+   var salt = crypto.randomBytes(128).toString('hex');
+   var dbString = hash(password, salt);
+   pool.query('INSERT INTO "user" (username, password) VALUES ($1, $2)', [username, dbString], function (err, result) {
+      if (err) {
           res.status(500).send(err.toString());
-        } else {
-            
-             res.send("User successfully created:" + username);
-         }
-        
-    });
+      } else {
+          res.send('User successfully created: ' + username);
+      }
+   });
 });
 
 app.post('/login', function(req, res) {
