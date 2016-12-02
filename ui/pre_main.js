0a3f56e61bf70c0e01f8e31e51af89e8e2aa09e6
T3lo <ritwik97bhattacharya@gmail.com>
2016-12-02 15:22:23 +0530

    [imad-console] Updates ui/pre_main.js

--- ui/pre_main.js
+++ ui/pre_main.js
@@ -122,8 +122,8 @@ btn.onclick = function () {
   };
   
   
-    var username = document.getElementById('username');
-    var password = document.getElementById('password');
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
     console.log(username);
     console.log(password);
     request.open('POST', 'http://t3lo.imad.hasura-app.io/login' , true);
