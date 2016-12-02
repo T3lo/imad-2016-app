a5d01c772d46087b56d6ba489890557c9ce1466e
T3lo <ritwik97bhattacharya@gmail.com>
2016-12-02 14:29:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -86,7 +86,7 @@ var htmlTemplate=`
 function hash(input ,salt){
     // How do we create a hash?
     var hashed = crypto.pbkdf2Sync(input ,salt ,10000 ,512 , 'sha512');
-    return ["pkbdf2" ,"10000", salt, hashed.toString()].join('$');
+    return ["pkbdf2" ,"10000", salt, hashed.toString('hex')].join('$');
 }
 
 app.get('/hash/:input', function(req, res) {
